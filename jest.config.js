c15921c02bb809fc983619c733d8366ea83251bc
danielamartinscosta <daniela.mc2011@hotmail.com>
2026-04-05 20:31:23 -0300

    fix: convert jest config to JS to avoid ts-node dependency

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,7 @@
-import type { Config } from 'jest'
-
-const config: Config = {
+/** @type {import('jest').Config} */
+module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.test.ts'],
   setupFiles: ['dotenv/config'],
 }
-
-export default config
